a353c17ed42b07de87822dfc8513b44a6e4cf608
Jcacero <jcruz2013c@gmail.com>
2022-04-04 00:47:08 -0300

    Contacto y equipo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { InicioComponent } from './page/inicio/inicio.component';
 import { TrabajoComponent } from './page/trabajo/trabajo.component';
 import { ServiciosComponent } from './page/servicios/servicios.component';
 import { ContactoComponent } from './page/contacto/contacto.component';
+import { EquipoComponent } from './page/equipo/equipo.component';
 const routes: Routes = [
   {path:'inicio',component:InicioComponent},
   {path:'',component:InicioComponent},
   {path:'trabajo',component:TrabajoComponent},
   {path:'servicios',component:ServiciosComponent},
-  {path:'contacto',component:ContactoComponent}
+  {path:'contacto',component:ContactoComponent},
+  {path:'equipo',component:EquipoComponent}
+
 
 ];
 
